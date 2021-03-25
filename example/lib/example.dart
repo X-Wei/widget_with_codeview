312684d3d9b648e26faa5af96f1d3ea07af18275
Ryan Edge <ryan@superformula.com>
2021-03-25 17:42:00 -0400

    chore: add example app

---
+++ example/lib/example.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Example extends StatelessWidget {
+  const Example({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("An example widget"),
+    );
+  }
+}
