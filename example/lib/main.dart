1be3ab89e6d12c36cf0963436047cb30192256e0
x-wei <wistar.sjtu@gmail.com>
2021-03-26 15:36:46 +0100

    #fixup remove unused variable in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,14 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
